088d5b35bcaa3d09a04f4c636469b5ee83f2bd8f
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2021-03-30 13:30:05 +0700

    yezzir

--- latihan_1/src/App.js
+++ latihan_1/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Assets/style/style.css";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        
+      </Container>
+
+      <div className="circle1"></div>
+      <div className="circle2"></div>
     </div>
   );
 }
---
+++ latihan_1/src/components/sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class sidebar extends Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <div className="user">
+                    <img src={"../Assets"}></img>
+                </div>
+            </div>
+        )
+    }
+}
